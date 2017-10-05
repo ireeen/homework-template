2d2bff5cc06f3caf5d1012d9ae40f7bbb92c3bbe
Irina Shirokova <innnieo@gmail.com>
2017-10-05 17:36:39 +0300

    Update game.py

--- homework3/game.py
+++ homework3/game.py
@@ -2,7 +2,7 @@
 
 # `random` module is used to shuffle field, see:
 # https://docs.python.org/3/library/random.html#random.shuffle
-
+import random
 
 # Empty tile, there's only one empty cell on a field:
 EMPTY_MARK = 'x'
@@ -23,7 +23,11 @@ def shuffle_field():
     :return: list with 16 randomly shuffled tiles,
     one of which is a empty space.
     """
-    pass
+    # generate solved field field
+    field = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, EMPTY_MARK]
+    from random import shuffle
+    shuffle(field)
+    return field
 
 
 def print_field(field):
@@ -32,7 +36,12 @@ def print_field(field):
     :param field: current field state to be printed.
     :return: None
     """
-    pass
+
+    print('------------------------')
+    for i in range(0, len(field), 4):
+        prepared_line = [format(str(x)) for x in field[i:i + 4]]
+        print(prepared_line)
+    print('------------------------')
 
 
 def is_game_finished(field):
@@ -41,7 +50,12 @@ def is_game_finished(field):
     :param field: current field state.
     :return: True if the game is finished, False otherwise.
     """
-    pass
+    for i in range(1, len(field) - 1):
+        if field[i] == EMPTY_MARK or field[i - 1] == EMPTY_MARK:
+            return False
+        if field[i - 1] > field[i]:
+            return False
+    return True
 
 
 def perform_move(field, key):
@@ -52,7 +66,20 @@ def perform_move(field, key):
     :return: new field state (after the move).
     :raises: IndexError if the move can't me done.
     """
-    pass
+    mark_position = field.index(EMPTY_MARK)
+    next_mark_position = mark_position + MOVES[key]
+    if not (0 < next_mark_position < len(field)):
+        raise IndexError()
+    # left move restrictions
+    if mark_position in [0, 4, 8, 12] and key == 'a':
+        raise IndexError()
+    # right move restrictions
+    if mark_position in [3, 7, 11, 15] and key == 'd':
+        raise IndexError()
+    field_new = list(field)
+    field_new[mark_position], field_new[next_mark_position] =\
+        field_new[next_mark_position], field_new[mark_position]
+    return field_new
 
 
 def handle_user_input():
@@ -64,7 +91,11 @@ def handle_user_input():
         'd' - right
     :return: <str> current move.
     """
-    pass
+    allowed_moves = list(MOVES.keys())
+    move = ''
+    while move not in allowed_moves:
+        move = input('Next move  \n')
+    return move
 
 
 def main():
@@ -73,11 +104,25 @@ def main():
     It also calls other methods.
     :return: None
     """
-    pass
+    field = shuffle_field()
+    counter = 0
+    while not is_game_finished(field):
+        print_field(field)
+        move = handle_user_input()
+        try:
+            field = perform_move(field, move)
+            counter += 1
+        except IndexError:
+            print('Wait! The end of the field')
+    print('You won!')
+    print('Number of moves: ', counter)
+    print_field(field)
 
 
 if __name__ == '__main__':
     # See what this means:
     # http://stackoverflow.com/questions/419163/what-does-if-name-main-do
-
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('shutting down')
